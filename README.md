ff9b2ccdcef5d5e4919521b141adc0d49d1a99e5
Matthieu Walter <matthw@users.noreply.github.com>
2021-09-03 09:31:01 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# fs_switch_type_7_password_decoder
+# FS Switch type 7 password decoder
+
+this script decodes type 7 passwords on [FS](https://wwww.fs.com/) switch
+tested on a [S3700](https://www.fs.com/products/84912.html) model
+
+example:
+```
+% ./fs_pw_dec.py 091e333e434451542d4024162b51535c5945
+My_super_P4ssword
+```
+
---
+++ fs_pw_dec.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# decrypt fs.com type 7 passwords
+#
+# ex:  
+# % ./fs_pwd.py 091e333e434451542d4024162b51535c5945
+# My_super_P4ssword
+#
+# test on a FS-3700 switch
+# by @matth_walter
+
+
+import sys
+
+KEY = b"JoanCheungOscar\x00"
+
+
+def fs_pw_dec(password):
+    try:
+        enc_pwd = bytes.fromhex(password)
+    except ValueError:
+        return None
+
+    dec_pwd = ""
+
+    if enc_pwd[0] < 0x10:
+        salt = enc_pwd[0]
+    else:
+        salt = enc_pwd[0] - 6
+
+    for c in enc_pwd[1:]:
+        d = ((((c - KEY[(salt + (salt >> 4) * -0x10) % len(KEY)]) - salt) + 0xfd) % 0x7f + 0x21) % 0x7f
+        dec_pwd += chr(d)
+        salt += 1
+
+    return dec_pwd
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 2:
+        print("usage: %s <encrypted password>".format(sys.argv[0]))
+        sys.exit(0)
+
+    pw = fs_pw_dec(sys.argv[1])
+    if pw is None:
+        print("error decrypting %s"%sys.argv[1])
+        sys.exit(1)
+
+    print(pw)
+
+
